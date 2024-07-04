c1141b0123a1a9e6cd0388be5f716c0e16ee6838
MairemSo <souayah.mariem@gmail.com>
2024-07-04 15:19:34 +0200

    displayFunctionAdded_Home

---
+++ cssScript.js
@@ -0,0 +1,12 @@
+//-------Navbar Script--------
+const mobileNav = document.querySelector(".hamburger");
+const navbar = document.querySelector(".menubar");
+
+const toggleNav = () => {
+  navbar.classList.toggle("active");
+  mobileNav.classList.toggle("hamburger-active");
+};
+mobileNav.addEventListener("click", () => toggleNav());
+
+
+//----------------------
--- script.js
+++ script.js
@@ -1,12 +1,56 @@
-//-------Navbar Script--------
-const mobileNav = document.querySelector(".hamburger");
-const navbar = document.querySelector(".menubar");
+loadCSV('items.csv', (data) => {
+    const inventory = parseCSV(data);
+    console.log(inventory)
+    displayInventory(inventory)
+});
 
-const toggleNav = () => {
-  navbar.classList.toggle("active");
-  mobileNav.classList.toggle("hamburger-active");
-};
-mobileNav.addEventListener("click", () => toggleNav());
+function displayInventory(inventory){
+    const container = document.getElementById("cardsContainer")
+    inventory.forEach(item => {
+        const itemCard = document.createElement('div')
+        itemCard.classList.add('box')
+        itemCard.innerHTML= `
+        <div class="box-top">
+              <img
+                class="box-image"
+                src="${item.photo_url}"
+                alt="${item.name}"
+              />
+              <div class="title-flex">
+                <h3 class="box-title">Name:${item.name}</h3>
+              </div>
+              <p class="color">Color: ${item.color}</p>
+              <p class="color">Size: ${item.size}</p>
+              <p class="color">Price: ${item.price}</p>
+              <a href="#" class="button">Add To Cart</a>
+        </div>
+        `;
+        container.appendChild(itemCard)
+    });
+}
+function loadCSV(url, callback) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => callback(data))
+        .catch(error => console.error('Error loading CSV file:', error));
+}
+
+function parseCSV(data) {
+    const lines = data.split('\n');
+    const headers = lines[0].split(',').map(header => header.trim());
+    const items = [];
+
+    for (let i = 1; i < lines.length; i++) {
+        const line = lines[i].split(',');
+        if (line.length === headers.length) {
+            const item = {};
+            headers.forEach((header, index) => {
+                item[header] = line[index].trim();
+            });
+            items.push(item);
+        }
+    }
+    return items;
+}
 
 
-//----------------------
